347b480d84d61fcbe8c3d0c24557d759621bd55d
GeorgGD <giorgos_davak@hotmail.com>
2020-10-21 12:24:42 +0200

    Introduce HTMLParser

---
+++ src/main/java/com/wiw/core/webscraper/HTMLParser.java
@@ -0,0 +1,19 @@
+package com.wiw.core.webscraper;
+
+import org.openqa.selenium.WebDriver;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HTMLParser implements Webscraper{
+
+	private WebDriver webdriver;
+	
+	public HTMLParser(){		
+	}
+
+	@Override
+	public void parse() {
+		// TODO Auto-generated method stub
+	}
+	
+}
