afa2cd793c0c56867ede18d4a77d165900da0a2e
GeorgGD <giorgos_davak@hotmail.com>
2020-10-24 21:11:39 +0200

    Introduce setWebDriver

--- src/main/java/com/wiw/core/webscraper/Webscraper.java
+++ src/main/java/com/wiw/core/webscraper/Webscraper.java
@@ -1,11 +1,14 @@
 package com.wiw.core.webscraper;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.springframework.stereotype.Component;
 
 @Component
 public interface Webscraper{
-    
+
+	void setWebDriver(WebDriver webDriver);
+	
 	void goToUrl(String url);
 
 	String scrapElement(By identifier);
