689da795519d06bb72d680b1cb23e6d365737c81
GeorgGD <giorgos_davak@hotmail.com>
2020-10-24 22:20:25 +0200

    Introduce setupOptions

--- src/main/java/com/wiw/core/webscraper/WebScraper.java
+++ src/main/java/com/wiw/core/webscraper/WebScraper.java
@@ -1,5 +1,6 @@
 package com.wiw.core.webscraper;
 
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -9,5 +10,12 @@ public class WebScraper {
 	@Autowired
 	private DriverManager driverManager;
 
-	
+	private ChromeOptions setupOptions() {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless",
+							 "--disable-gpu",
+							 "--window-size=1920,1080",
+							 "--ignore-certificate-errors");
+		return options;
+	}
 }
