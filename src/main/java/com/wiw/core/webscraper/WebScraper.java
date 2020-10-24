6eb9634d02e2953d87c64d4f9121bae006e1e6fc
GeorgGD <giorgos_davak@hotmail.com>
2020-10-24 21:37:26 +0200

    Introduce WebScraper class

---
+++ src/main/java/com/wiw/core/webscraper/WebScraper.java
@@ -0,0 +1,13 @@
+package com.wiw.core.webscraper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WebScraper {
+
+	@Autowired
+	private DriverManager driverManager;
+
+	
+}
