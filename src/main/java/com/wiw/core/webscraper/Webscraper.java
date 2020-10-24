a89bbb609cebd98d5e0dbe6dc0c5e112afdcd0e0
GeorgGD <giorgos_davak@hotmail.com>
2020-10-24 20:36:19 +0200

    Introduce scrapElement

--- src/main/java/com/wiw/core/webscraper/Webscraper.java
+++ src/main/java/com/wiw/core/webscraper/Webscraper.java
@@ -1,10 +1,12 @@
 package com.wiw.core.webscraper;
 
+import org.openqa.selenium.By;
 import org.springframework.stereotype.Component;
 
 @Component
 public interface Webscraper{
     
 	void goToUrl(String url);
-	
+
+	String scrapElement(By identifier);
 }
