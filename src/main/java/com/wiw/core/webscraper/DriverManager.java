671f2d04c6de7be8c7ec8de3cd00ab19ef9173b3
GeorgGD <giorgos_davak@hotmail.com>
2020-10-26 16:22:15 +0100

    Add documentation

--- src/main/java/com/wiw/core/webscraper/DriverManager.java
+++ src/main/java/com/wiw/core/webscraper/DriverManager.java
@@ -4,14 +4,36 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.springframework.stereotype.Component;
 
+/**
+ * Main interface for all classes implementing a WebDriver solution
+ * @author Georgios Davakos
+ * @since 2020-10-26
+ */
 @Component
 public interface DriverManager{
 
+	/**
+	 * Assigns the webDriver value
+	 * @param webDriver The webdriver to use	
+	 */
 	void setWebDriver(WebDriver webDriver);
-	
+
+	/**
+	 * Takes the driver to the given url
+	 * @param url The url to use	
+	 */
 	void goToUrl(String url);
 
+	/**
+	 * Locates the element with the given identifier and 
+	 * collects the contents inside the element
+	 * @param identifier The identifier to use 
+	 * @return The contents inside the element	
+	 */
 	String scrapElement(By identifier);
 
+	/**
+	 * Shuts down the driver 
+	 */
 	void closeBrowser();
 }
