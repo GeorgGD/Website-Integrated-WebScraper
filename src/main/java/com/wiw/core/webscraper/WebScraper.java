ea6fec1d48f989590c4fe3016efa5081dc87e99d
GeorgGD <giorgos_davak@hotmail.com>
2020-10-26 15:01:52 +0100

    Introduce scrapElemById

--- src/main/java/com/wiw/core/webscraper/WebScraper.java
+++ src/main/java/com/wiw/core/webscraper/WebScraper.java
@@ -1,5 +1,6 @@
 package com.wiw.core.webscraper;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -37,4 +38,9 @@ public class WebScraper {
 		driverManager.setWebDriver(driver);
 	}
 
+	public String scrapElemById(String name) {
+		By identifier = By.id(name);
+		String content = driverManager.scrapElement(identifier);		
+		return content;
+	}
 }
