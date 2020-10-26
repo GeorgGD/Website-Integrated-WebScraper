2a7935cf9c62c74d6a7d41c5d15cf2e42c42110c
GeorgGD <giorgos_davak@hotmail.com>
2020-10-26 16:04:17 +0100

    Add documentation to all methods

--- src/main/java/com/wiw/core/webscraper/SeleniumDriver.java
+++ src/main/java/com/wiw/core/webscraper/SeleniumDriver.java
@@ -6,26 +6,42 @@ import org.openqa.selenium.WebElement;
 import org.springframework.stereotype.Component;
 
 /**
- * Class for 
+ * Class for managing seleniums WebDriver interface
+ * @author Georgios Davakos
+ * @since 2020-10-26
  */
 @Component
 class SeleniumDriver implements DriverManager{
 	
 	private static WebDriver webDriver;
-	
+
 	public SeleniumDriver() {
 	}
-
+	
+	/**
+	 * Assigns the webDriver value
+	 * @param webDriver The webdriver to use	
+	 */
 	@Override
 	public void setWebDriver(WebDriver webDriver) {
 		SeleniumDriver.webDriver = webDriver;
 	}
 	
+	/**
+	 * Takes the driver to the given url
+	 * @param url The url to use	
+	 */
 	@Override
 	public void goToUrl(String url) {
 		webDriver.get(url);
 	}
 
+	/**
+	 * Finds the element with the given identifier and 
+	 * collects the contents inside the element
+	 * @param identifier The identifier to use 
+	 * @return The contents inside the element	
+	 */
 	@Override
     public String scrapElement(By identifier) {
 		WebElement element = webDriver.findElement(identifier);
@@ -36,7 +52,10 @@ class SeleniumDriver implements DriverManager{
 		
 		return null;
 	}
-
+	
+	/**
+	 * Shuts down the driver 
+	 */
 	@Override
 	public void closeBrowser() {
 		if(webDriver != null) {
