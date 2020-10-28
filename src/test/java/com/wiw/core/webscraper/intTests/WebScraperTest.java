cf82fcee8c0b0cc879761243b92706714ffedaab
GeorgGD <giorgos_davak@hotmail.com>
2020-10-28 23:32:56 +0100

    Add extent report

--- src/test/java/com/wiw/core/webscraper/intTests/WebScraperTest.java
+++ src/test/java/com/wiw/core/webscraper/intTests/WebScraperTest.java
@@ -17,6 +17,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -26,16 +27,17 @@ public class WebScraperTest {
 	@Autowired
 	private WebScraper webScraper;
 
-	@Autowired
 	private ExtentReports extentReport;
 	
 	private ExtentSparkReporter spark;
-
+	private ExtentTest exTestCase;
+	
 	@Before
 	@Order(1)
 	public void extentReportSetup() {
+		extentReport = new ExtentReports();
 		spark = new ExtentSparkReporter("./extentReport.html");
-	   extentReport.attachReporter(spark);
+		extentReport.attachReporter(spark);
 	}
 	
 	@Before
@@ -58,25 +60,43 @@ public class WebScraperTest {
 
 	@After
 	public void createExtentReportPage() {
-		extentReport.flush();
+		//extentReport.flush();
 	}
 	
 	@Test
 	public void goToUrlTest() {
-		String expectedUrl = "https://www.georgiosdavakos.se";
-		webScraper.goToUrl(expectedUrl);
-		String url = webScraper.currentLocation();
-		expectedUrl = expectedUrl + "/";
-		assertEquals(expectedUrl, url);
-	}
+		exTestCase = extentReport.createTest("Go To URL Test", "Goes to the given website");
+		String expectedUrl;
+		String url = "";
+		try {
+			expectedUrl = "https://www.georgiosdavakos.se";
+			webScraper.goToUrl(expectedUrl);
+			url = webScraper.currentLocation();
+			expectedUrl = expectedUrl + "/";
+			assertEquals(expectedUrl, url);
+			exTestCase.log(Status.PASS, "Navigated to the correct url");
+			exTestCase.pass("Passed");
+			extentReport.flush();
+		} catch (AssertionError e) {
+			exTestCase.log(Status.FAIL, "Failed to navigate to " + url);
+			exTestCase.log(Status.INFO, "Assertion error " + e);
+			exTestCase.fail("Failed");
+			extentReport.flush();
+			throw new AssertionError(e);
+		}
 
-	@Test
+	}
+    
+	/*	@Test
 	public void scrapElemByIdTest() {
+		exTestCase = extentReport.createTest("Test 2");
 		String id = "h-Braattvetaomriskerochavkastning";
 		String expectedText = "Bra att veta om risker och avkastning.";
 		String url = "https://www.nordnet.se/se";
 		webScraper.goToUrl(url);
 		String actualText = webScraper.scrapElemById(id);
 		assertEquals(expectedText, actualText);
-	}
+		exTestCase.pass("PASSED");
+		extentReport.flush();
+		}*/
 }
