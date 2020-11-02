daa115826bfe5f1227b2ae884214b4f076b2b56b
GeorgGD <giorgos_davak@hotmail.com>
2020-11-02 23:48:51 +0100

    Introduce viewResolver

--- src/main/java/config/springConfig.java
+++ src/main/java/config/springConfig.java
@@ -1,10 +1,19 @@
 package config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan("com.wiw.core.webscraper")
 public class springConfig {
-	
+
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver vr = new InternalResourceViewResolver();
+		vr.setPrefix("/WEB-INF/");
+		vr.setSuffix(".jsp");
+		return vr;
+	}
 }
