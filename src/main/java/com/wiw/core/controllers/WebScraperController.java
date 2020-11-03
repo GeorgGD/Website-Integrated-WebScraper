fd664854cfcb313c245af5e76991ea1e68c05688
GeorgGD <giorgos_davak@hotmail.com>
2020-11-03 13:00:36 +0100

    Add requestMapping to scrap method

--- src/main/java/com/wiw/core/controllers/WebScraperController.java
+++ src/main/java/com/wiw/core/controllers/WebScraperController.java
@@ -4,6 +4,7 @@ import com.wiw.core.webscraper.WebScraper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,6 +14,7 @@ public class WebScraperController {
 	@Autowired
 	private WebScraper webScraper;
 
+	@RequestMapping("scrap")
 	public ModelAndView scrap(@RequestParam("url") String url, @RequestParam("scrapBy") String byident) {
 		
 		ModelAndView mav = new ModelAndView();
