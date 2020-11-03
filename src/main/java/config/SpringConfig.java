d78384f8e438c3b271dee910a7b16214a832294b
GeorgGD <giorgos_davak@hotmail.com>
2020-11-03 22:53:44 +0100

    Implement java configuration

--- src/main/java/config/SpringConfig.java
+++ src/main/java/config/SpringConfig.java
@@ -3,12 +3,14 @@ package config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-
+@EnableWebMvc
 @Configuration
-@ComponentScan({ "com.wiw.core.webscraper", "com.wiw.core.controller" })
-public class SpringConfig {
+@ComponentScan(basePackages = {"com.wiw.core.webscraper", "com.wiw.core.controllers"})
+public class SpringConfig implements WebMvcConfigurer {
 
 	@Bean
 	public InternalResourceViewResolver viewResolver() {
