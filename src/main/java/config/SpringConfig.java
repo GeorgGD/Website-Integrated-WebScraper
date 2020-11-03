ff2414a176780db77c6a9e5457b192c2c4ac9fa2
GeorgGD <giorgos_davak@hotmail.com>
2020-11-03 17:31:52 +0100

    Improve componentScap structure for finding components

--- src/main/java/config/SpringConfig.java
+++ src/main/java/config/SpringConfig.java
@@ -5,8 +5,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+
 @Configuration
-@ComponentScan("com.wiw.core.webscraper")
+@ComponentScan({ "com.wiw.core.webscraper", "com.wiw.core.controller" })
 public class SpringConfig {
 
 	@Bean
