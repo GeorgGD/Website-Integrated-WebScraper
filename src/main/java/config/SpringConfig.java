9df907f7e9bc2e2be1b08c512e15f79c261685b6
GeorgGD <giorgos_davak@hotmail.com>
2020-11-04 19:22:24 +0100

    Revert the implementation of EnableWebMvc

--- src/main/java/config/SpringConfig.java
+++ src/main/java/config/SpringConfig.java
@@ -3,14 +3,11 @@ package config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-@EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = {"com.wiw.core.webscraper", "com.wiw.core.controllers"})
-public class SpringConfig implements WebMvcConfigurer {
+public class SpringConfig {
 
 	@Bean
 	public InternalResourceViewResolver viewResolver() {
