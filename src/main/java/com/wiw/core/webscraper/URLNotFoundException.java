3621e0b5b4ec647d80a0848c8bee61c442df7cc2
GeorgGD <giorgos_davak@hotmail.com>
2020-11-05 16:50:16 +0100

    Introduce URLNotFoundException

---
+++ src/main/java/com/wiw/core/webscraper/URLNotFoundException.java
@@ -0,0 +1,17 @@
+package com.wiw.core.webscraper;
+
+import org.openqa.selenium.WebDriverException;
+
+public class URLNotFoundException extends Exception {
+
+	private String errorMsg;
+	private WebDriverException webDriverException;
+	
+	private static final long serialVersionUID = 1L;
+
+	public URLNotFoundException(String msg, WebDriverException e) {
+		this.errorMsg = msg;
+		this.webDriverException = e;
+	}
+	
+}
