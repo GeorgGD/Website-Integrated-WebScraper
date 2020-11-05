ed9ffbc761d44814979aee57ea1f101815702eb1
GeorgGD <giorgos_davak@hotmail.com>
2020-11-05 13:55:22 +0100

    Remove tasks list for scrap method

--- src/main/java/com/wiw/core/controllers/WebScraperController.java
+++ src/main/java/com/wiw/core/controllers/WebScraperController.java
@@ -14,12 +14,6 @@ public class WebScraperController {
 	@Autowired
 	private WebScraper webScraper;
 
-	/*
-	 * 1. Navigate to the url
-	 * 2. Figure out which scrapBy method to use
-	 * 3. Scrap the element
-	 * 4. Return the scraped element
-	 */
 	@RequestMapping("/scrap")
 	public ModelAndView scrap(@RequestParam("url") String url, @RequestParam("scrapBy") String byIdent, @RequestParam("attributeName") String attributeName) {
 		if(!webScraper.hasDriverSetup())
