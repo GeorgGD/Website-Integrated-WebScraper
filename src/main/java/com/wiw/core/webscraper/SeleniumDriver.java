48afaf514c969d65c78d5586cc20e1b4019f8498
GeorgGD <giorgos_davak@hotmail.com>
2020-11-05 17:01:17 +0100

    Remove unused package

--- src/main/java/com/wiw/core/webscraper/SeleniumDriver.java
+++ src/main/java/com/wiw/core/webscraper/SeleniumDriver.java
@@ -1,7 +1,6 @@
 package com.wiw.core.webscraper;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
