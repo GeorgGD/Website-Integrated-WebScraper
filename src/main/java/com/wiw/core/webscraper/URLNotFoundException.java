1c665d02527763c14edf7dbb72a885258c92f3d0
GeorgGD <giorgos_davak@hotmail.com>
2020-11-06 16:45:42 +0100

    Expand on documentation

--- src/main/java/com/wiw/core/webscraper/URLNotFoundException.java
+++ src/main/java/com/wiw/core/webscraper/URLNotFoundException.java
@@ -3,7 +3,10 @@ package com.wiw.core.webscraper;
 import org.openqa.selenium.WebDriverException;
 
 /**
- * This class is meant for when an attempt to navigate to a given url leads nowhere
+ * This class is meant for when an attempt to navigate to a given url leads nowhere.
+ * It can also be used for when a user doesn't provide a URL. 
+ * Either way you, the user of this exception must specify how this exception is used 
+ * in your the error message!
  * @author Georgios Davakos
  * @since 2020-11-05
  */
